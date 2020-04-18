acd7311353706eb6668c0663454f53d121300d15
Srini Anand <anandologist@gmail.com>
2020-04-18 11:18:36 -0400

    First commit

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Predicting Eutrophication in the Mississippi Delta",
+        "excerpt":"This is an interesting and novel approach to classifying nitrogen data into buckets. I hope you find it inspirational to try out-of-the box techniques to solving problems. Feedback is appreciated at anandologist@gmail.com.  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/predicting-eutrophication-ms-delta/",
+        "teaser": null
+      }]
